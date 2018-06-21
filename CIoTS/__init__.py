c2f5d95b23bf0c126420595f95923bacb8708481
marcuspappik <marcus.pappik@student.hpi.uni-potsdam.de>
2018-06-21 18:29:17 +0200

    added new experiments and visualization

--- CIoTS/__init__.py
+++ CIoTS/__init__.py
@@ -1,6 +1,6 @@
 from .generator import CausalTSGenerator, draw_graph
 from .pc_chen_algorithm import pc_chen, pc_chen_modified
-from .pc_incremental_algorithm import pc_incremental
+from .pc_incremental_algorithm import pc_incremental, pc_incremental_extensive
 from .evaluation import evaluate_edges
 from .p_estimation import var_order_select
 from .stat_tools import partial_corr, partial_corr_test
@@ -13,6 +13,7 @@ __all__ = [
     'pc_chen',
     'pc_chen_modified',
     'pc_incremental',
+    'pc_incremental_extensive',
     'evaluate_edges',
     'var_order_select',
     'partial_corr',
