7e991702f6f492180606340b54a09d12f4fb40a3
danthe <me@danolithe.com>
2018-11-05 14:06:31 +0100

    Implement PC1 algorithm (#16)
    * Implement PC1 algorithm
    
    * Change condition size limit

--- CIoTS/pc_incremental_algorithm.py
+++ CIoTS/pc_incremental_algorithm.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 import networkx as nx
 import numpy as np
 from CIoTS.pc_chen_algorithm import pc_chen_modified
@@ -56,8 +58,8 @@ def pc_incremental(indep_test, ts, alpha=0.05, max_p=20, start=0, steps=1,
 
         # step 2
         for x_t, x in product(present_nodes, new_nodes):
-            p_value = indep_test(data_matrix, x_t, x, set(),
-                                 corr_matrix=corr_matrix)
+            p_value, statistic = indep_test(data_matrix, x_t, x, set(),
+                                            corr_matrix=corr_matrix)
             if p_value <= alpha:
                 G.add_edge(x, x_t)
 
@@ -66,8 +68,8 @@ def pc_incremental(indep_test, ts, alpha=0.05, max_p=20, start=0, steps=1,
             in_set = set(G.predecessors(x_t))
             for x in in_set:
                 cond = in_set - set([x])
-                p_value = indep_test(data_matrix, x_t, x, cond,
-                                     corr_matrix=corr_matrix)
+                p_value, statistic = indep_test(data_matrix, x_t, x, cond,
+                                                corr_matrix=corr_matrix)
                 if p_value > alpha:
                     G.remove_edge(x, x_t)
 
@@ -90,6 +92,99 @@ def pc_incremental(indep_test, ts, alpha=0.05, max_p=20, start=0, steps=1,
         return nx.relabel_nodes(graphs[best_p], node_mapping), graphs, times, bics
     else:
         return nx.relabel_nodes(graphs[best_p], node_mapping)
+
+
+def pc_incremental_pc1(indep_test, ts, alpha=0.05, max_p=20, start=0, steps=1,
+                       ic='bic', patiency=1, verbose=False, **kwargs):
+    # precalculated information
+    dim = ts.shape[1]
+
+    # verbose information
+    graphs = {}
+    times = {}
+    bics = {}
+
+    # initial graph
+    present_nodes = range(dim)
+    if start > 0:
+        node_mapping, data_matrix = transform_ts(ts, start)
+        corr_matrix = np.corrcoef(data_matrix, rowvar=False)
+        start_time = time()
+        G = pc_chen_modified(indep_test, ts, start, alpha)
+        times[start] = time() - start_time
+        graphs[start] = nx.relabel_nodes(G.copy(), node_mapping)
+        bics[start] = _graph_ic(start, dim, data_matrix, G, ic)
+        best_bic = bics[start]
+        best_p = start
+    else:
+        G = nx.DiGraph()
+        G.add_nodes_from(present_nodes)
+        best_bic = np.inf
+        best_p = 0
+
+    no_imp = 0
+
+    # iteration step
+    for p in range(start+steps, max_p+1, steps):
+        start_time = time()
+        node_mapping, data_matrix = transform_ts(ts, p)
+        corr_matrix = np.corrcoef(data_matrix, rowvar=False)
+        new_nodes = range(p*dim, min(p+steps, max_p)*dim)
+
+        # step 1: Add new nodes
+        G.add_nodes_from(new_nodes)
+
+        # step 2: Connect new nodes if not unconditionally independent
+        for x_t, x in product(present_nodes, new_nodes):
+            p_value, statistic = indep_test(data_matrix, x_t, x, set(),
+                                            corr_matrix=corr_matrix)
+            if p_value <= alpha:
+                G.add_edge(x, x_t)
+
+        # step 3: Check all connected nodes
+        for x_t in present_nodes:
+            parents = list(set(G.predecessors(x_t)))
+            # Goes up to full neighborhood, perhaps limit this
+            max_cond_dim = float('inf')
+            condition_size = 0
+            # PC_1
+            while condition_size < max_cond_dim and condition_size < len(parents) - 1:
+                parent_stats = defaultdict(lambda: float('inf'))
+                for x in parents:
+                    other_parents = [e for e in parents if e != x]
+                    condition = other_parents[:condition_size]
+
+                    p_value, statistic = indep_test(data_matrix, x_t, x, condition,
+                                                    corr_matrix=corr_matrix)
+                    parent_stats[x] = min(parent_stats[x], np.abs(statistic))
+
+                    if p_value > alpha:
+                        G.remove_edge(x, x_t)
+                        del parent_stats[x]
+
+                parents = [k for k, v in sorted(parent_stats.items(), key=lambda v:v[1], reverse=True)]
+                condition_size += 1
+
+        # verbose information
+        graphs[p] = nx.relabel_nodes(G.copy(), node_mapping)
+        times[p] = time() - start_time
+        bics[p] = _graph_ic(p, dim, data_matrix, G, ic)
+
+        # early stopping
+        if bics[p] < best_bic:
+            best_bic = bics[p]
+            best_p = p
+            no_imp = 0
+        else:
+            no_imp += 1
+            if no_imp >= patiency:
+                break
+
+    if verbose:
+        return nx.relabel_nodes(graphs[best_p], node_mapping), graphs, times, bics
+    else:
+        return nx.relabel_nodes(graphs[best_p], node_mapping)
+    pass
 
 
 def pc_incremental_extensive(indep_test, ts, alpha=0.05, max_p=20, start=0,
@@ -134,8 +229,8 @@ def pc_incremental_extensive(indep_test, ts, alpha=0.05, max_p=20, start=0,
 
         # step 2
         for x_t, x in product(present_nodes, new_nodes):
-            p_value = indep_test(data_matrix, x_t, x, set(),
-                                 corr_matrix=corr_matrix)
+            p_value, statistic = indep_test(data_matrix, x_t, x, set(),
+                                            corr_matrix=corr_matrix)
             if p_value <= alpha:
                 G.add_edge(x, x_t)
 
@@ -147,8 +242,8 @@ def pc_incremental_extensive(indep_test, ts, alpha=0.05, max_p=20, start=0,
                 in_set = set(G.predecessors(x_t))
                 for x in in_set:
                     cond = in_set - set([x])
-                    p_value = indep_test(data_matrix, x_t, x, cond,
-                                         corr_matrix=corr_matrix)
+                    p_value, statistic = indep_test(data_matrix, x_t, x, cond,
+                                                    corr_matrix=corr_matrix)
                     if p_value > alpha:
                         G.remove_edge(x, x_t)
             num_edges = new_num_edges
@@ -217,8 +312,8 @@ def pc_incremental_subsets(indep_test, ts, alpha=0.05, max_p=20, start=0,
 
         # step 2
         for x_t, x in product(present_nodes, new_nodes):
-            p_value = indep_test(data_matrix, x_t, x, set(),
-                                 corr_matrix=corr_matrix)
+            p_value, statistic = indep_test(data_matrix, x_t, x, set(),
+                                            corr_matrix=corr_matrix)
             if p_value <= alpha:
                 G.add_edge(x, x_t)
 
@@ -231,8 +326,8 @@ def pc_incremental_subsets(indep_test, ts, alpha=0.05, max_p=20, start=0,
                 for x in in_set:
                     cond_max = in_set - set([x])
                     for cond in set(combinations(cond_max, subset_size)):
-                        p_value = indep_test(data_matrix, x_t, x, cond,
-                                             corr_matrix=corr_matrix)
+                        p_value, statistic = indep_test(data_matrix, x_t, x, cond,
+                                                        corr_matrix=corr_matrix)
                         if p_value > alpha:
                             G.remove_edge(x, x_t)
                             break
