44a21408612a69150cad6605aa732cb64a6e300d
Daniel Thevessen <danthe96@gmx.de>
2019-01-16 18:13:21 +0100

    Upload notebooks

---
+++ datagen.py
@@ -0,0 +1,23 @@
+import os
+import pickle
+from time import time
+import sys
+from CIoTS import *
+
+data_path = 'notebooks/ICML/icml_data_no_autocorr/'
+
+dim, in_edges, tau, autocorr, data_length = eval(sys.argv[1])
+
+runs = range(10)
+
+for run in runs:
+    generator = CausalTSGenerator(dimensions=dim, max_p=tau, data_length=data_length,
+                                  incoming_edges=in_edges, autocorrelation=autocorr)
+    start = time()
+    ts = generator.generate()
+    elapsed = time() - start
+
+    with open(data_path + f't={tau}_d={dim}_in={in_edges}_autocorr={autocorr}_{run}.pickle', 'wb') as f:
+        pickle.dump(generator, f)
+
+    print(dim, in_edges, tau, autocorr, data_length, run, ': ', elapsed)
