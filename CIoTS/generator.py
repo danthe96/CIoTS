f3d9450e6c31cdf6b1d6ef40ddd1ae55dd0387b0
Daniel Thevessen <danthe96@gmx.de>
2019-01-17 18:39:15 +0100

    Update generator

--- CIoTS/generator.py
+++ CIoTS/generator.py
@@ -6,7 +6,7 @@ import numpy as np
 import pandas as pd
 from statsmodels.tsa.vector_ar.var_model import is_stable
 from random import sample, seed
-from itertools import product
+from itertools import product, permutations
 
 
 def node_name(time_series, l):
@@ -70,17 +70,21 @@ class CausalTSGenerator:
 
     def generate_stable_graph(self):
         stable_var = False
-        i = 0
+        # i = 0
         while not stable_var:
-            VAR_exog = self._generate_graph()
+            VAR_exog = self._generate_random_graph()
             stable_var = is_stable(VAR_exog[::-1])
-            if not stable_var:
-                print(f'Graph not stable. Regenerating' + ('.' * (i % 3 + 1)) + '   ', end='\r')
-                i += 1
-        print()
+        #     if not stable_var:
+        #         print(f'Not stable ({i+1})', end='\r')
+        #         print(f'Graph not stable. Regenerating' + ('.' * (i % 3 + 1)) + '   ', end='\r')
+        #         i += 1
+        # print()
         self.VAR_exog = VAR_exog
+        # return i
 
     def _generate_graph(self):
+        c = 0.4
+
         self.graph = nx.DiGraph()
         node_ids = list(product([d for d in range(self.dimensions)],
                                 [l for l in reversed(range(self.max_p+1))]))
@@ -98,7 +102,7 @@ class CausalTSGenerator:
             if d == d_t:
                 remaining_edges -= 1
                 candidates.remove((d_p, self.max_p))
-                weight = np.random.choice([-0.75, 0.75])
+                weight = np.random.choice([-c, c])
                 # np.random.multivariate_normal(
                 # mean=[-0.75, 0.75], cov=0.1*np.ones((2, 2)))[random.randint(0, 1)]
                 self.graph.add_edge(node_name(d_p, self.max_p), node_name(d, 0),
@@ -107,7 +111,7 @@ class CausalTSGenerator:
             # autocorrelation
             if self.autocorrelation:
                 # candidates.remove((d, 1))
-                weight = np.random.choice([-0.75, 0.75])
+                weight = np.random.choice([-c, c])
                 # np.random.multivariate_normal(
                 # mean=[-0.75, 0.75], cov=0.1*np.ones((2, 2)))[random.randint(0, 1)]
                 self.graph.add_edge(node_name(d, 1), node_name(d, 0),
@@ -115,10 +119,43 @@ class CausalTSGenerator:
 
             picks = sample(candidates, remaining_edges)
             for candidate in picks:
-                weight = np.random.choice([-0.75, 0.75])
+                weight = np.random.choice([-c, c])
                 # np.random.multivariate_normal(
                 # mean=[-0.75, 0.75], cov=0.1*np.ones((2, 2)))[random.randint(0, 1)]
                 self.graph.add_edge(node_name(*candidate), node_name(d, 0), weight=weight)
+
+        adjacency = np.array(nx.adjacency_matrix(self.graph).todense())
+        VAR_exog = []
+        target = [self.max_p + d * self.length for d in range(self.dimensions)]
+        for i in range(self.max_p):
+            source = [i + d * self.length for d in range(self.dimensions)]
+            VAR_exog.append(adjacency[:, target][source].T)
+        return np.array(VAR_exog)
+
+    def _generate_random_graph(self):
+        c = 0.4
+
+        self.graph = nx.DiGraph()
+        node_ids = list(product([d for d in range(self.dimensions)],
+                                [l for l in reversed(range(self.max_p+1))]))
+        self.graph.add_nodes_from([node_name(*n) for n in node_ids])
+
+        candidates = list(product(permutations(np.arange(self.dimensions), 2), np.arange(self.max_p) + 1))
+        # Ensure max_p is utilized
+        d_t = np.random.choice(self.dimensions)
+        d_p = np.random.choice([d for d in range(self.dimensions) if d != d_t])
+        candidates.remove(((d_p, d_t), self.max_p))
+        self.graph.add_edge(node_name(d_p, self.max_p), node_name(d_t, 0), weight=np.random.choice([-c, c]))
+
+        edge_idxs = np.random.choice(len(candidates), size=(self.incoming_edges*self.dimensions - 1,))
+        for edge_idx in edge_idxs:
+            (from_d, to_d), tau = candidates[edge_idx]
+            self.graph.add_edge(node_name(from_d, tau), node_name(to_d, 0), weight=np.random.choice([-c, c]))
+
+        if self.autocorrelation:
+            for d in range(self.dimensions):
+                weight = np.random.choice([-c, c])
+                self.graph.add_edge(node_name(d, 1), node_name(d, 0), weight=weight)
 
         adjacency = np.array(nx.adjacency_matrix(self.graph).todense())
         VAR_exog = []
